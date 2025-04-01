18034f94196fbdfbd4d9af66d2e490a1adfcd72e
melwin <114172287+itzTedx@users.noreply.github.com>
2025-04-01 12:26:13 +0530

    Implement Redis client, authentication schemas, and session management

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,5 +1,36 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import type {
+  BuildQueryResult,
+  DBQueryConfig,
+  ExtractTablesWithRelations,
+} from "drizzle-orm";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle({ client: sql });
+import * as schema from "./schema";
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+export const db = drizzle(pool, {
+  schema,
+});
+
+type Schema = typeof schema;
+type TSchema = ExtractTablesWithRelations<Schema>;
+
+export type IncludeRelation<TableName extends keyof TSchema> = DBQueryConfig<
+  "one" | "many",
+  boolean,
+  TSchema,
+  TSchema[TableName]
+>["with"];
+
+export type InferResultType<
+  TableName extends keyof TSchema,
+  With extends IncludeRelation<TableName> | undefined = undefined,
+> = BuildQueryResult<
+  TSchema,
+  TSchema[TableName],
+  {
+    with: With;
+  }
+>;
--- src/server/schema.ts
+++ src/server/schema.ts
@@ -1,8 +1,18 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+export const UserRoles = ["admin", "user"] as const;
+export type UserRole = (typeof UserRoles)[number];
+export const userRoleEnum = pgEnum("user_role", UserRoles);
+
+export const UsersTable = pgTable("users", {
+  id: uuid().primaryKey().defaultRandom(),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  password: text("password").notNull(),
+  salt: text("salt").notNull(),
+  role: userRoleEnum().notNull().default("user"),
+  createdAt: timestamp("createdAt").defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdate(() => new Date()),
 });
